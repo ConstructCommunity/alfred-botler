b74d21341edf2e6dd6b2242cdb81002b807675eb
Quentin Goinaud <armaldio@gmail.com>
2018-04-09 09:07:12 +0200

    first update to support job posting

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ require('pretty-error').start();
 const documentation = require('./routes/documentation');
 const donation      = require('./routes/donation');
 const api           = require('./routes/api');
-const hook           = require('./routes/hook');
+const hook          = require('./routes/hook');
 const commands      = require('./routes/commands');
 const dashboard     = require('./routes/dashboard');
 
@@ -60,22 +60,22 @@ const DiscordStrategy = require('passport-discord').Strategy;
 const scopes = ['identify', 'email', 'guilds'];
 
 passport.serializeUser(function (user, done) {
-    done(null, user);
+  done(null, user);
 });
 passport.deserializeUser(function (obj, done) {
-    done(null, obj);
+  done(null, obj);
 });
 
 const discordStrat = new DiscordStrategy({
-    clientID    : CONSTANTS.BOT.CLIENT_ID,
-    clientSecret: CONSTANTS.BOT.CLIENT_SECRET,
-    callbackURL : isDev ? 'http://localhost:80/api/discord/callback' : 'https://alfred.armaldio.xyz/api/discord/callback',
-    scope       : scopes
+  clientID    : CONSTANTS.BOT.CLIENT_ID,
+  clientSecret: CONSTANTS.BOT.CLIENT_SECRET,
+  callbackURL : isDev ? 'http://localhost:80/api/discord/callback' : 'https://alfred.armaldio.xyz/api/discord/callback',
+  scope       : scopes
 }, (accessToken, refreshToken, profile, done) => {
-    profile.refreshToken = refreshToken; // store this for later refreshes
-    process.nextTick(function () {
-        return done(null, profile);
-    });
+  profile.refreshToken = refreshToken; // store this for later refreshes
+  process.nextTick(function () {
+    return done(null, profile);
+  });
 });
 passport.use(discordStrat);
 refresh.use(discordStrat);
@@ -83,47 +83,53 @@ refresh.use(discordStrat);
 console.log('temp directory', os.tmpdir());
 
 let filestore = new FileStore({
-    path: os.tmpdir()
+  path: os.tmpdir()
 });
 
 let memorystore = new MemoryStore({
-    checkPeriod: 86400000
+  checkPeriod: 86400000
 });
 
 app.use(session({
-    resave           : true,
-    saveUninitialized: true,
-    store            : isDev ? filestore : memorystore,
-    secret           : 'alfred is awesome'
+  resave           : true,
+  saveUninitialized: true,
+  store            : isDev ? filestore : memorystore,
+  secret           : 'alfred is awesome'
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(function (req, res, next) {
-    res.locals.bot   = bot;
-    res.locals.guild = bot.guilds.get(CONSTANTS.GUILD_ID);
-    next();
+  res.locals.bot   = bot;
+  res.locals.guild = bot.guilds.get(CONSTANTS.GUILD_ID);
+  next();
 });
 
 function checkAuth (req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
 }
 
 function empty (a, b, next) {
-    next();
+  next();
 }
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', 'https://cc_forms.armaldio.xyz');
+  next();
+});
 
 /* GET home page. */
 app.get('/', function (req, res) {
-    res.render('home');
+  res.render('home');
 });
 app.get('/login', passport.authenticate('discord', {scope: scopes}), function (req, res) {
-    res.locals.bot   = bot;
-    res.locals.guild = res.locals.bot.guilds.get(CONSTANTS.GUILD_ID);
+  res.locals.bot   = bot;
+  res.locals.guild = res.locals.bot.guilds.get(CONSTANTS.GUILD_ID);
 });
 app.use('/doc', documentation);
 app.use('/donation', donation);
@@ -134,20 +140,20 @@ app.use('/dashboard', checkAuth, dashboard);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    let err    = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  let err    = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error   = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error   = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 /**
@@ -157,94 +163,94 @@ app.use(function (err, req, res, next) {
 const sms = require('free-mobile-sms-api');
 
 sms.on('sms:error', e => {
-    console.info(e.code + ': ' + e.msg);
+  console.info(e.code + ': ' + e.msg);
 });
 
 sms.on('sms:success', data => {
-    console.info('Success! :D');
+  console.info('Success! :D');
 });
 
 bot = new Bot({
-    commandPrefix  : '!',
-    owner          : CONSTANTS.OWNER,
-    disableEveryone: true
+  commandPrefix  : '!',
+  owner          : CONSTANTS.OWNER,
+  disableEveryone: true
 });
 
 let getConnectedUsers = function () {
-    const guild = bot.guilds.get(CONSTANTS.GUILD_ID);
+  const guild = bot.guilds.get(CONSTANTS.GUILD_ID);
 
-    const guildMembers = guild.members;
+  const guildMembers = guild.members;
 
-    const connectedUsers = guildMembers.filter(member => {
-        return (member.presence.status !== 'offline');
-    });
+  const connectedUsers = guildMembers.filter(member => {
+    return (member.presence.status !== 'offline');
+  });
 
-    return connectedUsers.size;
+  return connectedUsers.size;
 };
 
 let updateStatus = function () {
-    const users = getConnectedUsers();
-    bot.user.setActivity(`${users} users`, {
-        type: 'WATCHING'
-    });
+  const users = getConnectedUsers();
+  bot.user.setActivity(`${users} users`, {
+    type: 'WATCHING'
+  });
 };
 
 process.on('uncaughtException', err => {
-    console.info('Uncaugh');
-    console.info('Caught exception: ' + err);
-    console.info('Stack : ', err.stack);
-    bot.emit('disconnect', {
-        code    : 1000,
-        reason  : 'Process: Uncaught exception',
-        wasClean: true
-    });
-    process.exit();
+  console.info('Uncaugh');
+  console.info('Caught exception: ' + err);
+  console.info('Stack : ', err.stack);
+  bot.emit('disconnect', {
+    code    : 1000,
+    reason  : 'Process: Uncaught exception',
+    wasClean: true
+  });
+  process.exit();
 });
 
 process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
-    Raven.captureException(reason);
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  Raven.captureException(reason);
 });
 
 bot.on('presenceUpdate', () => {
-    updateStatus();
+  updateStatus();
 }).on('message', async message => {
-    try {
-        function isOnline (id) {
-            const user = bot.guilds.get(CONSTANTS.GUILD_ID).members.get(id);
-            return (user.presence.status !== 'offline');
-        }
-
-        if (message.mentions.users.exists('id', CONSTANTS.OWNER) || message.content.toLowerCase()
-                                                                           .includes('armaldio')) {
-            if (!isOnline(CONSTANTS.OWNER)) {
-                const text = `[DISCORD] #${message.channel.name} @${message.author.username} said: ${message.content.trim()}`;
-                sms.account(95222614, 'OWoVbMFNlat344');
-                sms.send(encodeURIComponent(text));
-            }
-        }
-
-        try {
-            let x = await bot.parse(message);
-        } catch (err) {
-            console.log(err);
-        }
-
-    } catch (e) {
-        Raven.captureException(e);
-        console.log('ERROR PARSING MESSAGE', e);
+  try {
+    function isOnline (id) {
+      const user = bot.guilds.get(CONSTANTS.GUILD_ID).members.get(id);
+      return (user.presence.status !== 'offline');
     }
-}).on('disconnect', closeEvent => {
-    console.info('BOT DISCONNECTING');
-    bot.login(CONSTANTS.BOT.TOKEN);
-    console.info('Close Event : ', closeEvent);
-}).on('ready', () => {
-    console.log('Bot ready');
-    updateStatus();
 
-    bot.checkBlogsAndUpdates();
-    bot.updateMessage();
-    bot.checkNewPlugins();
+    if (message.mentions.users.exists('id', CONSTANTS.OWNER) || message.content.toLowerCase()
+                                                                       .includes('armaldio')) {
+      if (!isOnline(CONSTANTS.OWNER)) {
+        const text = `[DISCORD] #${message.channel.name} @${message.author.username} said: ${message.content.trim()}`;
+        sms.account(95222614, 'OWoVbMFNlat344');
+        sms.send(encodeURIComponent(text));
+      }
+    }
+
+    try {
+      let x = await bot.parse(message);
+    } catch (err) {
+      console.log(err);
+    }
+
+  } catch (e) {
+    Raven.captureException(e);
+    console.log('ERROR PARSING MESSAGE', e);
+  }
+}).on('disconnect', closeEvent => {
+  console.info('BOT DISCONNECTING');
+  bot.login(CONSTANTS.BOT.TOKEN);
+  console.info('Close Event : ', closeEvent);
+}).on('ready', () => {
+  console.log('Bot ready');
+  updateStatus();
+
+  bot.checkBlogsAndUpdates();
+  bot.updateMessage();
+  bot.checkNewPlugins();
 
 });
 
@@ -252,5 +258,5 @@ console.log('isDev: ', isDev);
 
 isDev ? bot.login(CONSTANTS.BOT.DEVTOKEN) : bot.login(CONSTANTS.BOT.TOKEN);
 app.listen(port, ip, () => {
-    console.log('Server running on http://%s:%s', ip, port);
+  console.log('Server running on http://%s:%s', ip, port);
 });
--- routes/hook.js
+++ routes/hook.js
@@ -6,19 +6,54 @@ const CONSTANTS = require('../constants');
 const passport = require('passport');
 
 router.post('/job', (req, res, next) => {
-        let bot   = res.locals.bot;
-        let guild = bot.guilds.get(CONSTANTS.GUILD_ID);
+    let bot   = res.locals.bot;
+    let guild = bot.guilds.get(CONSTANTS.GUILD_ID);
 
-        let channels = guild.channels.array().filter(x => x.type === 'text');
-        let users    = guild.members.array();
+    let channels = guild.channels.array().filter(x => x.type === 'text');
+    let users    = guild.members.array();
 
-        console.log(req.body);
-        guild.channels.get('244447929400688650').send(`A NEW JOB BY ${req.body.user} HAS BEEN POSTED`, {
-            embed: {
-                'description': JSON.stringify(req.body)
-            }
-        });
-        res.sendStatus(200);
-    }
+    console.log(req.body);
+    let { body } = req;
+
+    guild.channels.get('244447929400688650').send(`A NEW JOB BY ${req.body.user} HAS BEEN POSTED`, {
+      embed: {
+        'description': body.title,
+        'color'      : 11962861,
+        'footer'     : {
+          'text': 'Create your own offers now by visiting: https://cc_forms.armaldio.xyz.'
+        },
+        'thumbnail'  : {
+          'url': 'https://cdn.discordapp.com/attachments/244447929400688650/429264763940503552/joboffericon.png'
+        },
+        'author'     : {
+          'name'    : `NEW OFFER BY ${body.user}`,
+          'icon_url': 'https://cdn.discordapp.com/attachments/244447929400688650/328647581984882709/AlfredBotlerSmall.png'
+        },
+        'fields'     : [
+          {
+            'name' : CONSTANTS.MESSAGE.SEPARATOR,
+            'value': CONSTANTS.MESSAGE.EMPTY
+          },
+          {
+            'name' : 'Offer Conditions:',
+            'value': `- ${body.announceType}
+- ${body.paymentType}
+- ${body.paymentDetails}
+- ${body.contact}
+ᅠ`
+          },
+          {
+            'name' : 'Offer Details:',
+            'value': body.details
+          },
+          {
+            'name' : CONSTANTS.MESSAGE.EMPTY,
+            'value': CONSTANTS.MESSAGE.SEPARATOR
+          }
+        ]
+      }
+    });
+    res.sendStatus(200);
+  }
 );
 module.exports = router;
