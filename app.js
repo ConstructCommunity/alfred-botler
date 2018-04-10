4ea37f63dcc1f18d6d705be242830c249a9b83da
Quentin Goinaud <armaldio@gmail.com>
2018-04-11 00:40:16 +0200

    whitelist domain

--- app.js
+++ app.js
@@ -46,7 +46,18 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
-app.use(cors());
+const whitelist = ['http://localhost:8080/', 'https://cc_jobs.armaldio.xyz/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
