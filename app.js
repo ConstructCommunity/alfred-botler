d9b865c4cdbebc0a1b37d8a6de8e50db32be8033
Quentin Goinaud <armaldio@gmail.com>
2018-05-23 10:11:10 +0000

    potential fi for alfred posting messages in club channel

--- app.js
+++ app.js
@@ -244,7 +244,7 @@ bot.on('presenceUpdate', () => {
     }
 
     try {
-      if (message.webhookID === null && message.channel.id === CONSTANTS.CHANNELS.CREATIONCLUB) {
+      if (message.webhookID === null && message.author.id !== CONSTANTS.BOT.ID && message.channel.id === CONSTANTS.CHANNELS.CREATIONCLUB) {
         let msgText = message.content;
         let owner   = message.author;
         owner.send(`**Join the Construct Creation Club by visiting the following link:** https://lnk.armaldio.xyz/WebCreationClub`);
