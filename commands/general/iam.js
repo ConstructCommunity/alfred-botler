67f18d6e74e0e15beb3053ddfa415cc82d2a6fb7
Quentin Goinaud <armaldio@gmail.com>
2019-01-09 19:53:55 -0500

    add iam command

--- commands/general/iam.js
+++ commands/general/iam.js
@@ -41,76 +41,58 @@ export default class iam extends Command {
     return hasPermissions(this.client, permissions, msg);
   }
 
-  run(msg, { role }) {
-    switch (role) {
-      case 'god':
-        return msg.reply('Sorry, Armaldio is our only god ...');
+  // eslint-disable-next-line
+  async run(msg, { role }) {
+    const fun = [
+      ['god', 'Sorry, Armaldio is our only god ...'],
+      ['ashley', 'No, Ash is too busy adding features ;)'],
+      ['armaldio', 'No, you are not.\nOr maybe you are. I don\'t know.'],
+      ['helper', 'Helper role is an important thing. You must request it to a Staff member.'],
+    ];
 
-      case 'ashley':
-        return msg.reply('Oh, glad to see you here');
+    let found = false;
+    fun.forEach((entry) => {
+      console.log(entry);
+      if (entry[0] === role) {
+        msg.reply(entry[1]);
+        found = true;
+      }
+    });
 
-      case 'armaldio':
-        return msg.reply('No, you are not.\nOr maybe you are. I don\'t know.');
+    if (found) return;
 
-      case 'helper':
-        return msg.reply('Helper role is an important thing. You must request it to a Staff member.');
-
-      case 'a dog':
-        return msg.reply('You should ask <@155322717422354432> to be sure');
-      default:
-        // eslint-disable-next-line
-        let ret;
-
-        if (typeof roles[role] === 'undefined') {
-          msg.reply('Sorry, this role is invalid');
-          ret = this.usage(msg);
-        } else if (msg.member.roles.has(roles[role])) {
-          msg.member.removeRole(roles[role]);
-          ret = msg.channel.send({
-            embed: {
-              description: CONSTANTS.MESSAGE.EMPTY,
-              color: 15844367,
-              footer: {
-                text: '(Request custom roles by mentioning or PMing a staff member!)',
-              },
-              thumbnail: {
-                url: 'https://cdn.discordapp.com/attachments/244447929400688650/331776445955309568/ROLEREMiconsmall.png',
-              },
-              author: {
-                name: `YOU NO LONGER HAVE THE ${role.toUpperCase()} ROLE!`,
-                icon_url: 'https://cdn.discordapp.com/attachments/244447929400688650/328647581984882709/AlfredBotlerSmall.png',
-              },
-              fields: [
-                {
-                  name: 'Available Roles (https://lnk.armaldio.xyz/cc_roles):',
-                  value: Object.keys(roles).join(', '),
-                },
-              ],
-            },
-          });
-        } else {
-          msg.member.addRole(roles[role]);
-          ret = msg.channel.send({
-            embed: {
-              description: CONSTANTS.MESSAGE.EMPTY,
-              color: 15844367,
-              thumbnail: {
-                url: 'https://cdn.discordapp.com/attachments/244447929400688650/331776445947052042/ROLEGETiconsmall.png',
-              },
-              author: {
-                name: `YOU NOW HAVE THE ${role.toUpperCase()} ROLE!`,
-                icon_url: 'https://cdn.discordapp.com/attachments/244447929400688650/328647581984882709/AlfredBotlerSmall.png',
-              },
-              fields: [
-                {
-                  name: 'Available Roles (https://lnk.armaldio.xyz/cc_roles):',
-                  value: Object.keys(roles).join(', '),
-                },
-              ],
-            },
-          });
-        }
-        return ret;
+    let icon = '';
+    if (typeof roles[role] === 'undefined') {
+      await msg.reply('Sorry, this role is invalid');
+    } else if (msg.member.roles.has(roles[role])) {
+      await msg.member.removeRole(roles[role]);
+      icon = 'ROLEREMiconsmall';
+    } else {
+      await msg.member.addRole(roles[role]);
+      icon = 'ROLEGETiconsmall';
     }
+
+    await msg.channel.send({
+      embed: {
+        description: CONSTANTS.MESSAGE.EMPTY,
+        color: 15844367,
+        footer: {
+          text: '(Request custom roles by mentioning or PMing a staff member!)',
+        },
+        thumbnail: {
+          url: `https://cdn.discordapp.com/attachments/244447929400688650/331776445947052042/${icon}.png`,
+        },
+        author: {
+          name: `YOU NOW HAVE THE ${role.toUpperCase()} ROLE!`,
+          icon_url: 'https://cdn.discordapp.com/attachments/244447929400688650/328647581984882709/AlfredBotlerSmall.png',
+        },
+        fields: [
+          {
+            name: 'Available Roles (https://lnk.armaldio.xyz/cc_roles):',
+            value: Object.keys(roles).join(', '),
+          },
+        ],
+      },
+    });
   }
 }
