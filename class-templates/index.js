38189cc273708e3fbc7802ee2e149833f7f84c0a
Quentin Goinaud <armaldio@gmail.com>
2019-01-11 00:35:26 -0500

    -

--- class-templates/index.js
+++ class-templates/index.js
@@ -1,9 +1,7 @@
+export { default as Blog } from './Announcement_Blog';
+
 export { default as RoleToggle } from './Announcement_RoleToggle';
 export { default as RoleHelp } from './Announcement_RoleHelp';
 
-export { default as Blog } from '../class-templates/Announcement_Blog';
-
 export { default as C2Update } from './Announcement_C2';
 export { default as C3Update } from './Announcement_C3';
-
-export { default as Info } from './Announcement_Info';
