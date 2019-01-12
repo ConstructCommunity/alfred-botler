b0d3869eeb47117772c890b8b40dd02ea718133d
Quentin Goinaud <armaldio@gmail.com>
2019-01-11 23:54:24 -0500

    -

--- templates/index.js
+++ templates/index.js
@@ -1,9 +1,15 @@
 export { default as Blog } from './Announcement_Blog';
 
-export { default as RoleToggle } from './Announcement_RoleToggle';
-export { default as RoleHelp } from './Announcement_RoleHelp';
+export { default as Bug } from './Announcement_Bug';
 
 export { default as C2Update } from './Announcement_C2';
 export { default as C3Update } from './Announcement_C3';
 
-export { default as Bug } from './Announcement_Bug';
+export { default as Info } from './Announcement_Info';
+
+export { default as JobOfferCommunity } from './Announcement_JobOfferCommunity';
+export { default as JobOfferInfo } from './Announcement_JobOfferInfo';
+export { default as JobOfferMain } from './Announcement_JobOfferMain';
+
+export { default as RoleToggle } from './Announcement_RoleToggle';
+export { default as RoleHelp } from './Announcement_RoleHelp';
