0f23cf7885bba542ec28739c3702b0b3c634adf4
Quentin Goinaud <armaldio@gmail.com>
2019-01-15 23:45:59 -0500

    -

--- socket.js
+++ socket.js
@@ -12,7 +12,7 @@ const options = {
 };
 
 const handler = (req, res) => {
-  console.log('Server running');
+  console.log(`Server running in ${isDev ? 'development' : 'production'}`);
 
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello World!');
