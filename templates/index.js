c3c9a69df54e38376eb8b616efc789c3ba85bfc7
TheRealDannyyy <dannyyy55fly@googlemail.com>
2019-01-30 00:37:42 +0100

    Update index.js

--- templates/index.js
+++ templates/index.js
@@ -7,17 +7,9 @@ export { default as C3Update } from './Announcement_C3';
 
 export { default as Info } from './Announcement_Info';
 
-export { default as JobOfferCommunity } from './Announcement_JobOfferCommunity';
-export { default as JobOfferInfo } from './Announcement_JobOfferInfo';
-export { default as JobOfferMain } from './Announcement_JobOfferMain';
-
 export { default as RoleHelp } from './Announcement_RoleHelp';
 export { default as RoleToggle } from './Announcement_RoleToggle';
 
-export { default as Promo } from './Announcement_Promo';
-export { default as PromoApp } from './Announcement_PromoApp';
-export { default as PromoDeny } from './Announcement_PromoDeny';
-export { default as PromoDown } from './Announcement_PromoDown';
 export { default as PromoUp } from './Announcement_PromoUp';
 
 export { default as Notify } from './Announcement_Notify';
