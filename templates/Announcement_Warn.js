e14487e4d28268c3960c788bbc8939526a52ec65
TheRealDannyyy <dannyyy55fly@googlemail.com>
2019-02-03 02:07:17 +0100

    Update Announcement_Warn.js

--- templates/Announcement_Warn.js
+++ templates/Announcement_Warn.js
@@ -16,6 +16,6 @@ export default class Warn extends Template {
       .setThumbnail(`${CONSTANTS.GITHUB.RAW_REPO_URL_PREFIX}/assets/mini/Negativeicon.png`)
       .setAuthor('YOU JUST RECEIVED A WARNING!', `${CONSTANTS.GITHUB.RAW_REPO_URL_PREFIX}/assets/mini/AlfredBotlericon.png`)
       .addField(CONSTANTS.MESSAGE.SEPARATOR, CONSTANTS.MESSAGE.EMPTY)
-      .addField('What are my options?', 'You can ask for support by contacting the CC Staff', false);
+      .addField('What are my options?', 'You can ask for support by contacting the CC Staff.', false);
   }
 }
