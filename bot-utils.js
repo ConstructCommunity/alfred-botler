94b8ee0e9f1845391b063f6e7dae6c7d24ebcf46
TheRealDannyyy <dannyyy55fly@googlemail.com>
2019-03-25 21:51:55 +0100

    Update bot-utils.js
    test

--- bot-utils.js
+++ bot-utils.js
@@ -186,6 +186,7 @@ export const checkC2Updates = async (client) => {
       });
 
       await database.ref('c2release').set(newVersion);
+      await sent.react(':voteup:');
     }
   } catch (error) {
     console.log(error);
