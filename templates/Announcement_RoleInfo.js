2084530cba2d40d02c836724861f47a5a7824ee8
TheRealDannyyy <dannyyy55fly@googlemail.com>
2019-08-11 23:54:19 +0200

    Update Announcement_RoleInfo.js

--- templates/Announcement_RoleInfo.js
+++ templates/Announcement_RoleInfo.js
@@ -26,7 +26,7 @@ export default class RoleHelp extends Template {
       fields: [
 	    {
           name: 'Exclusive Roles:',
-          value: '**Nitro Supporter:** Members supporting the server using Discord Nitro.\nᅠ',
+          value: '**Nitro Supporter:** Members server boosting with Nitro.\nᅠ',
         },
         {
           name: 'Roles For Members:',
