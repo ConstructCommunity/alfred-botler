dfad5a34cbcb604acb0b6a0c6718bb52f384a9cd
TheRealDannyyy <dannyyy55fly@googlemail.com>
2019-08-11 22:17:23 +0200

    RoleInfo Template Test

---
+++ templates/Announcement_RoleInfo.js
@@ -0,0 +1,41 @@
+import CONSTANTS from '../constants';
+
+import Template from './Template';
+
+export default class RoleHelp extends Template {
+  constructor(variables) {
+    super('role-help', variables, {
+      roles: {
+        role1: '111',
+        role2: '222',
+      },
+    });
+  }
+
+  embed() {
+    return {
+      description: CONSTANTS.MESSAGE.SEPARATOR,
+      color: 11962861,
+      thumbnail: {
+        url: `${CONSTANTS.GITHUB.RAW_REPO_URL_PREFIX}/assets/mini/Infoicon.png`,
+      },
+      author: {
+        name: 'Construct Community Role List',
+        icon_url: `${CONSTANTS.GITHUB.RAW_REPO_URL_PREFIX}/assets/mini/DiscordNotifyicon.png`,
+      },
+      fields: [
+        {
+          name: 'Roles For Members:',
+          value: '**RoleName:** !iam rolename\n**RoleName:** !iam rolename\n**RoleName:** !iam rolename\n**RoleName:** !iam rolename',
+        },
+        {
+          name: 'Roles With Application Process:',
+          value: '**RoleName:** !iam rolename\n**RoleName:** !iam rolename\n**RoleName:** !iam rolename\n**RoleName:** !iam rolename',
+        },
+      ],
+	  footer: {
+        text: CONSTANTS.MESSAGE.SCIRRA_FOOTER,
+      },
+    };
+  }
+}
