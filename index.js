83ddad834e9fca1de9d9c7d98ac06d29d34c29d8
Quentin Goinaud <armaldio@gmail.com>
2019-09-26 21:46:15 +0200

    move firebase to the file that use it

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 require('dotenv').config();
 require('@babel/register');
 require('@babel/polyfill');
-
-const firebase = require('firebase');
-
-const config = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-};
-firebase.initializeApp(config);
-
 require('./app.js');
