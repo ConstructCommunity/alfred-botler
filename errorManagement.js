8766ca1b45905247d9cbad08f97881b1f06a5905
TheRealDannyyy <dannyyy55fly@googlemail.com>
2020-02-27 00:43:22 +0100

    Update errorManagement.js

--- errorManagement.js
+++ errorManagement.js
@@ -2,8 +2,7 @@ import constants from './constants';
 
 export const genericError = (err, message, args, from, Pattern, result) => {
   const text = `**__An error occured!__**
-
-**Error: \`${err.message}\`
+**Error:** \`${err.message}\`
 **Context**: \`${message.content}\`
 `;
 
