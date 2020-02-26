337a24af6354d1262f9da1b36bdeaaf17d3c70a1
Quentin Goinaud <armaldio@gmail.com>
2020-02-27 00:30:46 +0100

    fix promo channel

--- commands/everyone/promo.js
+++ commands/everyone/promo.js
@@ -42,7 +42,7 @@ export default class promo extends Command {
       return;
     }
 
-    const sent = await duplicateMessage(msg, CONSTANTS.CHANNELS.PRIVATE_TESTS, (content) => content.replace(/!promo ?/, ''));
+    const sent = await duplicateMessage(msg, CONSTANTS.CHANNELS.PROMO, (content) => content.replace(/!promo ?/, ''));
 
     // send pending approval notification
     await msg.author.send({
