13876b30464fe5bfbb63e226c61363b6f9bfd311
TheRealDannyyy <dannyyy55fly@googlemail.com>
2021-02-13 16:06:10 +0100

    Update Announcement_Ask.ts
    Remove english language code.

--- templates/Announcement_Ask.ts
+++ templates/Announcement_Ask.ts
@@ -18,7 +18,7 @@ export default class Ask extends Template {
       .addFields([
         { name: CONSTANTS.MESSAGE.SEPARATOR, value: '**Things you can do before asking for help:**' },
         { name: '<:C3:561134796911280128> Construct 3 Manual:', value: '[Read Manual ➚](https://www.construct.net/make-games/manuals/construct-3)', inline: true },
-        { name: '<:C2:561134777445646336> Construct 2 Manual:', value: '[Read Manual ➚](https://www.construct.net/en/construct-2/manuals/construct-2)', inline: true },
+        { name: '<:C2:561134777445646336> Construct 2 Manual:', value: '[Read Manual ➚](https://www.construct.net/construct-2/manuals/construct-2)', inline: true },
       ])
       .addFields([
         { name: CONSTANTS.MESSAGE.SEPARATOR, value: '**Tips for getting quick help:**' },
