6bbaa3968ae54f76c232d1704c5ef9e424c94acf
Quentin Goinaud <quentin.goinaud@gmail.com>
2026-01-19 13:11:20 +0100

    fix

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,8 @@
-import firebase from 'firebase/app';
-import 'firebase/database'
+import firebase from "firebase/app";
+import "firebase/database";
+
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config = {
 	apiKey: process.env.apiKey,
@@ -7,10 +10,9 @@ const config = {
 	databaseURL: process.env.databaseURL,
 	storageBucket: process.env.storageBucket,
 	messagingSenderId: process.env.messagingSenderId,
-}
+};
 
 firebase.initializeApp(config);
 
 export const database = firebase.database();
-export default firebase
-
+export default firebase;
